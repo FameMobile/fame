e47574b1255742808f3de1f11dee06dfb3c79a64
jcohen28 <jcohen28@gmail.com>
2017-04-10 01:04:40 -0500

    remove pods from github

--- Pods/Target Support Files/Pods-fameTests/Pods-fameTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_fameTests : NSObject
-@end
-@implementation PodsDummy_Pods_fameTests
-@end
--- Pods/Target Support Files/Pods-fameUITests/Pods-fameUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_fameUITests : NSObject
-@end
-@implementation PodsDummy_Pods_fameUITests
-@end
